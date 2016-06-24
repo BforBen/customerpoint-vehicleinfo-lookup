96d05ee13bc595a1374d5268feb24dfded0c0ba9
Ben <BforBen@users.noreply.github.com>
2016-06-24 15:41:56 +0100

    Add caching

--- CustomerPoint.VehicleInfo.Lookup/Lookup.cs
+++ CustomerPoint.VehicleInfo.Lookup/Lookup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Runtime.Caching;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CustomerPoint.VehicleInfo.Models;
@@ -19,21 +20,42 @@ namespace CustomerPoint.VehicleInfo
 
             RegistrationNumber = Regex.Replace(RegistrationNumber, @"[^A-Z\d]+", "", RegexOptions.None);
 
-            var ApiClient = new CarRegSoapClient();
-            var Spec = await ApiClient.CheckAsync(RegistrationNumber, Properties.Settings.Default.RegCheckApiKey);
+            VehicleData vd = null;
 
-            if (Spec != null)
+            var Cache = MemoryCache.Default;
+            var CacheName = "VehicleInfo-" + RegistrationNumber;
+
+            // Check if data is cached already
+            if (!Cache.Contains(CacheName))
             {
-                return new VehicleData
+                var ApiClient = new CarRegSoapClient();
+                var Spec = await ApiClient.CheckAsync(RegistrationNumber, Properties.Settings.Default.RegCheckApiKey);
+
+                if (Spec != null)
+                {
+                    vd = new VehicleData
                     {
                         Vrm = RegistrationNumber,
                         Make = Spec.vehicleData.MakeDescription.Value,
                         Model = Spec.vehicleData.ModelDescription.Text.FirstOrDefault(),
                         Fuel = Spec.vehicleData.FuelType.CurrentTextValue.Text.FirstOrDefault()
                     };
-            }
 
-            return null;
+                    // Cache result for 1 day
+                    var Cached = Cache.Add(
+                            new CacheItem(CacheName, vd),
+                            new CacheItemPolicy()
+                                {
+                                    AbsoluteExpiration = System.DateTimeOffset.UtcNow.AddDays(1)
+                                });
+                }
+
+                return null;
+            }
+            else
+            {
+                return (VehicleData)Cache.Get(CacheName);
+            }
         }
     }
 }
