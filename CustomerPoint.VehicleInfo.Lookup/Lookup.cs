ecb7dec0a348e9b1f482aebeb1ab9299dd398126
Ben <BforBen@users.noreply.github.com>
2016-06-24 14:27:14 +0100

    Add AppVeyor config

---
+++ CustomerPoint.VehicleInfo.Lookup/Lookup.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CustomerPoint.VehicleInfo.Models;
+using CustomerPoint.VehicleInfo.RegCheckApi;
+
+namespace CustomerPoint.VehicleInfo
+{
+    public static class Lookup
+    {
+        public static async Task<VehicleData> Vehicle(string RegistrationNumber)
+        {
+            if (!string.IsNullOrWhiteSpace(RegistrationNumber))
+            {
+                return null;
+            }
+
+            RegistrationNumber = RegistrationNumber.ToUpper();
+
+            RegistrationNumber = Regex.Replace(RegistrationNumber, @"[^A-Z\d]+", "", RegexOptions.None);
+
+            var ApiClient = new CarRegSoapClient();
+            var Spec = await ApiClient.CheckAsync(RegistrationNumber, Properties.Settings.Default.RegCheckApiKey);
+
+            if (Spec != null)
+            {
+                return new VehicleData
+                    {
+                        Vrm = RegistrationNumber,
+                        Make = Spec.vehicleData.MakeDescription.Value,
+                        Model = Spec.vehicleData.ModelDescription.Text.FirstOrDefault(),
+                        Fuel = Spec.vehicleData.FuelType.CurrentTextValue.Text.FirstOrDefault()
+                    };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CustomerPoint.VehicleInfo.Lookup/Models/VehicleData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerPoint.VehicleInfo.Models
+{
+    public class VehicleData
+    {
+        [Display(Name = "Registration number")]
+        public string Vrm { get; internal set; }
+        public string Make { get; internal set; }
+        public string Model { get; internal set; }
+        public string Fuel { get; internal set; }
+    }
+}
